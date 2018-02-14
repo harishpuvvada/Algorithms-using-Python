abc317a0aa622507bd9749b1015c8e357628e4e2
harishpuvvada <hp1047@nyu.edu>
2018-02-14 17:07:17 -0500

    commit suicide problem

--- 0-KnapSack-Weight,Value.py
+++ 0-KnapSack-Weight,Value.py
@@ -1,3 +1,7 @@
+#A naive recursive implementation of 0-1 Knapsack Problem
+
+# Returns the maximum value that can be put in a knapsack of capacity W
+
 def knapSack(W , wt , val , n):
 
     # Base Case
