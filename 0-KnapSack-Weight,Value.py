7ab9453c08e59b8cb59e66953877227c374bdb6c
harishpuvvada <hp1047@nyu.edu>
2018-03-04 17:19:04 -0500

    kp 0-1 using DP

--- 0-KnapSack-Weight,Value.py
+++ 0-KnapSack-Weight,Value.py
@@ -1,8 +1,7 @@
 #A naive recursive implementation of 0-1 Knapsack Problem
 
 # Returns the maximum value that can be put in a knapsack of capacity W
-
-def knapSack(W , wt , val , n):
+def knapSack(W , wt , val , n):   #Time Complexity is O(2^n)
 
     # Base Case
     if n == 0 or W == 0 :
@@ -13,17 +12,34 @@ def knapSack(W , wt , val , n):
     if (wt[n-1] > W):
         return knapSack(W , wt , val , n-1)
 
-    # return the maximum of two cases:
-    # (1) nth item included
-    # (2) not included
     else:
         return max(val[n-1] + knapSack(W-wt[n-1] , wt , val , n-1),knapSack(W , wt , val , n-1))
+        #      max(---- eq for including nth wt-------------------,---- for not including------)
 
-# end of function knapSack
 
-# To test above function
+def dynamicProg_KP(W , wt , val , n):  #Time Complexity is O(nW)
+
+    #creating a table using list comprehensions
+    k = [[0 for i in range(W+1)] for j in range(n+1)]  #here "+1" cuz we go from 0 to W,0 to n
+
+
+    for i in range(n+1):
+        for w in range(W+1):
+            # Base Case
+            if i == 0 or w == 0 :
+                k[i][w] = 0
+            elif(wt[n-1] <= W):
+                k[i][w] = max(val[i-1] + k[i-1][w-wt[i-1]],k[i-1][w])
+                #      max(---- eq for including nth wt-------------------,---- for not including------)
+            else:
+                k[i][w] = k[i-1][w]
+    print(k) #print table
+    return k[n][W]
+
+# To test above functions
 val = [60, 100, 120]
 wt = [10, 20, 30]
 W = 50
 n = len(val)
-print knapSack(W , wt , val , n)
+print(dynamicProg_KP(W , wt , val , n))
+print(knapSack(W , wt , val , n))
