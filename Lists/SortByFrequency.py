b62985f260c025428241397008472c35a66bc7f7
harishpuvvada <hp1047@nyu.edu>
2018-03-25 22:48:52 -0400

    sort by freq

---
+++ Lists/SortByFrequency.py
@@ -0,0 +1,19 @@
+'''
+You are given an array with duplicates. You have to sort the array with decreasing frequency of elements. If two elements have the same frequency, sort them by their actual value in increasing order.
+Ex: [2 3 5 3 7 9 5 3 7]
+Output: [3 3 3 5 5 7 7 2 9]'''
+
+def sortByFreq():
+    
+
+
+
+
+
+
+    return
+
+
+
+arr = [2 3 5 3 7 9 5 3 7]
+print(sortByFreq())
