fbcd404b3b790a954bcfc0ca0a9833d669a5c5cd
harishpuvvada <hp1047@nyu.edu>
2018-03-25 23:14:22 -0400

    height of a tree

---
+++ Trees/heightOfATree.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self,data,):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class maxDepth(node): #Time complexity is O(number of nodes)
+
+    if node is None:
+        return
+    else:
+
+        right_depth = maxDepth(node.right)
+        left_depth = maxDepth(node.left)
+
+    return max(right_depth,left_depth)
+
+
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+
+print ("Height of tree is %d" %(maxDepth(root)))
