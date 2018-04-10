f70d3a674ad45495ab4d45defe3fa0eaaf7fc18f
harishpuvvada <hp1047@nyu.edu>
2018-04-09 20:21:14 -0400

    stacks,queues

---
+++ Lists/Queue-UsingTwoStacks.py
@@ -0,0 +1,40 @@
+'''
+Note:
+stack - reverses the order of input elements while output
+Queue - prints in same order. So,
+
+We use in_stack to fill in with elements and then pop them in to out_stack.Then we have in the given order of elements. So now we can return the poped element from out_stack
+'''
+
+
+class Queue2Stacks(object):
+
+    def __init__(self):
+        # Two Stacks
+        self.in_stack = []
+        self.out_stack = []
+
+    def enqueue(self, element):
+
+        self.in_stack.append(element)
+        pass
+
+    def dequeue(self):
+
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack.pop()
+        pass
+
+
+
+q = Queue2Stacks()
+
+for i in range(5):
+    q.enqueue(i)
+
+for i in range(5):
+    print (q.dequeue())
+
+#output will be 0,1,2,3,4
